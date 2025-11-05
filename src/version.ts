f55d9017ce1c7ff1e1bebc8a76046f0a4f6c681d
kiwiscanwifi <intelliservenz@gmail.com>
2025-11-05 20:32:06 +1300

    Create version.ts

---
+++ src/version.ts
@@ -0,0 +1,14 @@
+/**
+ * @file src/version.ts
+ * @project fireCNC
+ * @author Mark Dyer
+ * @location Blenheim, New Zealand
+ * @contact intelliservenz@gmail.com
+ *
+ * @description
+ * Defines application and firmware version constants for consistent use across the app.
+ */
+export const versions = {
+  FIRMWARE_VERSION: '0.0.4',
+  FIRMWARE_RELEASE_DATE: '2024-08-06'
+};
